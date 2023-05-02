f003cfdacc41db9aaba88b54830d7a22f41e70bf
artsid <90769533+milkyWRLD@users.noreply.github.com>
2023-05-02 23:14:35 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Menu Open Close
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+menu.onclick=()=>{
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+};
+// Swiper
+var swiper = new Swiper(".services-content", {
+    spaceBetween: 30,
+    centerSlides: true,
+    autoplay: {
+        delay: 50000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+// Header
+let header = document.querySelector('header')
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
